c2f8e102b847c219a293f0a84e0e891ba2b60bcb
Neha Shoaib <thisisnehashoaib@gmail.com>
2026-09-22 02:04:17 +0500

    Simplify Tailwind CSS config by removing theme
    Removed type definition and theme customization from Tailwind CSS configuration.

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,32 +1,9 @@
-import type { Config } from "tailwindcss";
-
-const config: Config = {
-  content: [
+content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    // Keep these just in case you aren't using a src folder
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
-  theme: {
-    extend: {
-      colors: {
-        jarHappiness: "#FCD34D",
-        jarGrief: "#312E81",
-        jarSorrow: "#64748B",
-        jarPain: "#9F1239",
-        jarDisappointment: "#94A3B8",
-        jarSadness: "#0F766E",
-        jarAnxious: "#86EFAC",
-        jarAnger: "#EA580C",
-        jarHope: "#10B981",
-        jarGratitude: "#C084FC",
-      },
-      fontFamily: {
-        arabic: ['"Amiri"', "serif"],
-        urdu: ['"Noto Nastaliq Urdu"', "serif"],
-        sans: ['"Inter"', "sans-serif"],
-      }
-    },
-  },
-  plugins: [],
-};
-export default config;
